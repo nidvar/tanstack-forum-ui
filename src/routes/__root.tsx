2c6e4519ef37b53dde067424fabd89aec38f56df
nidvar <djayshine87@hotmail.com>
2025-10-20 16:02:23 +1100

    page not found

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,8 @@ import {
     Outlet, 
     createRootRouteWithContext 
 } from '@tanstack/react-router';
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query';
+import { Link } from '@tanstack/react-router';
 
 import Header from '../components/Header';
 
@@ -13,6 +14,7 @@ type RouterContext = {
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootLayout,
+  notFoundComponent: NotFound
 })
 
 function RootLayout(){
@@ -21,6 +23,17 @@ function RootLayout(){
             <HeadContent />
             <Header />
             <Outlet />
+        </>
+    )
+}
+
+function NotFound(){
+    return (
+        <>
+            <div className='main center'>
+                <h1>Page Not Found</h1>
+                <Link to='/' className='link-button'>HOME</Link>
+            </div>
         </>
     )
 }
