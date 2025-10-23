bb1291c68a5d03d5424e8d84a23b89176c56189c
nidvar <djayshine87@hotmail.com>
2025-10-23 12:37:04 +1100

    new post form

--- src/routes/posts/new/index.tsx
+++ src/routes/posts/new/index.tsx
@@ -1,4 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+
+import { useState, useEffect } from 'react';
 
 export const Route = createFileRoute('/posts/new/')({
     head: ()=> ({
@@ -8,5 +10,91 @@ export const Route = createFileRoute('/posts/new/')({
 })
 
 function NewPostPage() {
-    return <div>New Topic</div>
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [tags, setTags] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = function(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+
+        const tagsArray: string[] = [];
+
+        if(title == '' || summary == '' || content == ''){
+            return;
+        }
+        if(tags != ''){
+            const arr = tags.split(',');
+            arr.forEach((item)=>{
+                tagsArray.push(item)
+            });
+        }
+        const formData = {
+            title,
+            summary,
+            content,
+            tags: [...tagsArray]
+        };
+
+        console.log(formData);
+    }
+
+    return (
+        <>
+            <div className='main'>
+                <h1>Create New Post</h1>
+                <form className='new-post-form' onSubmit={handleSubmit}>
+                    <label
+                        htmlFor='title'
+                    >
+                        Title
+                    </label>
+                    <input
+                        id='title'
+                        type='text'
+                        value={title}
+                        onChange={function(e){setTitle(e.target.value)}}
+                    />
+
+                    <label
+                        htmlFor='summary'
+                    >
+                        Summary
+                    </label>
+                    <input
+                        id='summary'
+                        type='text'
+                        value={summary}
+                        onChange={function(e){setSummary(e.target.value)}}
+                    />
+
+                    <label
+                        htmlFor='content'
+                    >
+                        Content
+                    </label>
+                    <textarea
+                        id='content'
+                        value={content}
+                        onChange={function(e){setContent(e.target.value)}}
+                    ></textarea>
+
+                    <label
+                        htmlFor='tags'
+                    >
+                        Tags (optional)
+                    </label>
+                    <input
+                        placeholder='comma separated'
+                        id='tags'
+                        type='text'
+                        value={tags}
+                        onChange={function(e){setTags(e.target.value)}}
+                    />
+
+                    <button className='normal-button'>CREATE</button>
+                </form>
+            </div>
+        </>
+    )
 }
