70113775223ddb53e5f131cf18cccd623ab4bf36
nidvar <djayshine87@hotmail.com>
2025-11-02 09:23:01 +1100

    global state added to profile page

--- src/routes/profile/$profileId/index.tsx
+++ src/routes/profile/$profileId/index.tsx
@@ -1,11 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { useAuth } from '../../../store/authContext';
+
+import { useState, useEffect } from 'react';
+
 export const Route = createFileRoute('/profile/$profileId/')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
     const { profileId } = Route.useParams();
+
+    const authState = useAuth();
+
+    useEffect(()=>{
+        console.log(authState);
+    }, []);
+
     return (
         <>
             <div className='main margin-top-xl'>
