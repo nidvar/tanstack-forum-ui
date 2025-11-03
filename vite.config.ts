4ca8e66dba975050c1aad35e33ea94681e7c0326
nidvar <djayshine87@hotmail.com>
2025-11-03 15:25:07 +1100

    deployment prep

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,5 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, './src'),
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),  // remove "/api" prefix
-      },
-    },
-  },
+  }
 })
